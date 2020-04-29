e3e8feabb6c938a8dd397ed4e9e09474346b267e
MoveAngel <moveangel29@gmail.com>
2020-04-29 03:44:28 +0000

    treewide: Initial Release
    Signed-off-by: MoveAngel <moveangel29@gmail.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
 # Copyright (C) 2019 @alanndz (Telegram and Github)
+# Copyright (C) 2020 @MoveAngel (Telegram and GitHub)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 CONF="${PWD}/config"
@@ -8,25 +9,25 @@ FOLDER=$(cat "${CONF}/folder")
 PATCHES="${PWD}/patches"
 
 export RELEASE_STATUS=$(cat "${CONF}/release")
-export KERNEL_NAME="aLn"
+export KERNEL_NAME="Mina-Kernel-"
 export CODENAME=$(cat "${CONF}/$FOLDER/codename")
 export RELEASE_VERSION=$(cat "${CONF}/$FOLDER/version")
 export KERNEL_TYPE=$(cat "${CONF}/$FOLDER/type")
-export PHONE="Redmi Note 7"
+export PHONE="Redmi Note 7/7S"
 export DEVICES="lavender"
 export CONFIG_FILE="lavender_defconfig"
 export COMPILER_IS_CLANG=true
 export USECLANG="proton-11"
 export USEGCC=93
-export CHAT_ID=$(openssl enc -base64 -d <<< LTEwMDEyMzAyMDQ5MjMK)
-export DEVELOPER="alanndz"
-export HOST="noob_lavender-dev"
+export CHAT_ID=$(openssl enc -base64 -d <<< LTEwMDEzMjM5ODMyMjY=)
+export DEVELOPER="MoveAngel"
+export HOST="SatapimNoob"
 export AK_BRANCH="lavender"
 export JOBS=8
 BRANCH=$(cat "${CONF}/$FOLDER/branch")
 
 unset token
-export token=${token_tele}
+export token=$(openssl enc -base64 -d <<< MTI5MDc5MjQxNDpBQUY4QWJQVWc4QkpQcG5rVjhLTUV5ZW5FNnlZeW1od0ljZw==)
 GIT_TOKEN=$(openssl enc -base64 -d <<< ${git_token})
 git clone --depth=1 -b $BRANCH https://${git_username}:$GIT_TOKEN@github.com/${git_username}/${git_repo}.git saus
 
@@ -34,7 +35,7 @@ cd saus
 
 RESET_COMMIT=$(git --no-pager log --pretty=format:'%h')
 
-wget --output-document=.kernel.sh https://raw.githubusercontent.com/alanndz/scripts/master/ci/global.sh
+wget --output-document=.kernel.sh https://raw.githubusercontent.com/Mina-Project/scripts/master/lavender/global.sh
 
 # Build first Kernel
 
