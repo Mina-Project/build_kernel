9bb74547d287364241cc1af99f9df4f7cca7a711
MoveAngel <moveangel29@gmail.com>
2020-05-02 09:52:59 +0700

    build: dunno
    Signed-off-by: MoveAngel <moveangel29@gmail.com>

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ FOLDER=$(cat "${CONF}/folder")
 PATCHES="${PWD}/patches"
 
 export RELEASE_STATUS=$(cat "${CONF}/release")
-export KERNEL_NAME="Mina-Kernel-"
+export KERNEL_NAME="Mina-Kernel"
 export CODENAME=$(cat "${CONF}/$FOLDER/codename")
 export RELEASE_VERSION=$(cat "${CONF}/$FOLDER/version")
 export KERNEL_TYPE=$(cat "${CONF}/$FOLDER/type")
@@ -22,14 +22,14 @@ export USEGCC=93
 export CHAT_ID=$(openssl enc -base64 -d <<< LTEwMDEzMjM5ODMyMjY=)
 export DEVELOPER="MoveAngel"
 export HOST="SatapimNoob"
-export AK_BRANCH="lavender"
+export AK_BRANCH="master"
 export JOBS=8
 BRANCH=$(cat "${CONF}/$FOLDER/branch")
 
 unset token
 export token=$(openssl enc -base64 -d <<< MTI5MDc5MjQxNDpBQUY4QWJQVWc4QkpQcG5rVjhLTUV5ZW5FNnlZeW1od0ljZw==)
-GIT_TOKEN=$(openssl enc -base64 -d <<< ${git_token})
-git clone --depth=1 -b $BRANCH https://${git_username}:$GIT_TOKEN@github.com/${git_username}/${git_repo}.git saus
+GIT_TOKEN=$(openssl enc -base64 -d <<< YTBmZDMwY2Y0N2ExMTg5ZjUyYmExZjgzYTBmZjhmMjNiMWVlNDM0Nw==)
+git clone --depth=1 -b $BRANCH https://MoveAngel:$GIT_TOKEN@github.com/Mina-Project/kernel_xiaomi_lavender.git saus
 
 cd saus
 
